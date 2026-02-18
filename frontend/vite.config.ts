36332c09139f9b08f5dcf1513a9198a1ef30f277
Nathan Alam <nathanalam99@gmail.com>
2026-02-18 17:28:36 -0500

    Move to Netlify for deploys

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/interlock-api/" : "/",
+export default defineConfig(() => ({
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
